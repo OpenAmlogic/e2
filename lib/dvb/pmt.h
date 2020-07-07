d5045901d1eee00881cd8ed482298f440823d06b
thestormbreaker <33662974+thestormbreaker@users.noreply.github.com>
2020-07-07 15:07:26 +0200

    Update pmt.h

--- lib/dvb/pmt.h
+++ lib/dvb/pmt.h
@@ -3,16 +3,14 @@
 
 #ifndef SWIG
 #include <map>
-#include <set>
 #include <lib/base/buffer.h>
-#include <lib/components/file_monitor.h>
 #include <lib/dvb/idvb.h>
 #include <lib/dvb/dvb.h>
 #include <lib/dvb/idemux.h>
 #include <lib/dvb/esection.h>
+#include <lib/dvb/cahandler.h>
+#include <lib/dvb/pmtparse.h>
 #include <lib/python/python.h>
-#include <dvbsi++/program_map_section.h>
-#include <dvbsi++/program_association_section.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -20,90 +18,70 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#include <boost/any.hpp>
-
-class eDVBCAService;
 class eDVBScan;
-
-struct channel_data: public sigc::trackable
- {
-	ePtr<eDVBChannel> m_channel;
-	ePtr<eConnection> m_stateChangedConn;
-	int m_prevChannelState;
-	int m_dataDemux;
-};
-
-typedef std::map<eServiceReferenceDVB, eDVBCAService*, CompareService> CAServiceMap;
-typedef std::map<iDVBChannel*, channel_data*> ChannelMap;
-
-class eDVBCAServiceConn;
-typedef std::list<eFileWatch *> FileWatchList;
-typedef std::map<std::string, eDVBCAServiceConn*> CAServiceConnMap;
-typedef std::set<std::string> CASocketSet;
-
-class eDVBCAService: public sigc::trackable
-{
-	friend class eDVBCAServiceConn;
-
-	eServiceReferenceDVB m_service;
-	uint8_t m_used_demux[32];
-
-	bool m_first;
-	unsigned char m_capmt[2048];
-
-	void recheckConnections();
-	void sendCAPMT();
-
-	static void DVBChannelAdded(eDVBChannel*);
-	static void DVBChannelStateChanged(iDVBChannel*);
-	static CAServiceMap exist;
-	static ChannelMap exist_channels;
-	static ePtr<eConnection> m_chanAddedConn;
-	static channel_data *getChannelData(eDVBChannelID &chid);
-	static FileWatchList active_watches;
-	static CASocketSet available_sockets;
-
-	CAServiceConnMap active_connections;
-
-	eDVBCAService();
-	~eDVBCAService();
-public:
-	static void fileWatchEventCB(eFileWatch *, eFileEvent);
-	static void registerChannelCallback(eDVBResourceManager *res_mgr);
-	static RESULT register_service( const eServiceReferenceDVB &ref, int demux_nums[2], eDVBCAService *&caservice );
-	static RESULT unregister_service( const eServiceReferenceDVB &ref, int demux_nums[2], eTable<ProgramMapSection> *ptr, int tuner_no );
-	void buildCAPMT(eTable<ProgramMapSection> *ptr, bool haveCaIds, int tuner_no);
-};
 
 #endif
 
-class eDVBServicePMTHandler: public sigc::trackable
+class OCSection : public LongCrcSection
+{
+	protected:
+		void *data;
+
+	public:
+		OCSection(const uint8_t * const buffer)
+		: LongCrcSection(buffer)
+		{
+			data = malloc(getSectionLength());
+			memcpy(data, buffer, getSectionLength());
+		}
+		~OCSection()
+		{
+			free(data);
+		}
+		void *getData() { return data; }
+};
+
+#include <list>
+#include <string>
+class HbbTVApplicationInfo
+{
+public:
+	int m_OrgId;
+	int m_AppId;
+	int m_ControlCode;
+	short m_ProfileCode;
+	std::string m_HbbTVUrl;
+	std::string m_ApplicationName;
+public:
+	HbbTVApplicationInfo(int controlCode, int orgid, int appid, std::string hbbtvUrl, std::string applicationName,int profileCode)
+		: m_OrgId(orgid), m_AppId(appid), m_ControlCode(controlCode), m_ProfileCode(profileCode),
+		m_HbbTVUrl(hbbtvUrl), m_ApplicationName(applicationName)
+	{}
+};
+typedef std::list<HbbTVApplicationInfo *> HbbTVApplicationInfoList;
+typedef HbbTVApplicationInfoList::iterator HbbTVApplicationInfoListIterator;
+typedef HbbTVApplicationInfoList::const_iterator HbbTVApplicationInfoListConstIterator;
+
+class eDVBServicePMTHandler: public eDVBPMTParser
 {
 #ifndef SWIG
 	friend class eDVBCAService;
+	friend class eRTSPStreamClient;
 	eServiceReferenceDVB m_reference;
 	ePtr<eDVBService> m_service;
-
-	static std::map<uint32_t, uint8_t> sdt_versions;
-	static bool ddp_support;
-	static bool truehd_support;
 
 	int m_last_channel_state;
 	eDVBCAService *m_ca_servicePtr;
 	ePtr<eDVBScan> m_dvb_scan; // for sdt scan
 
-	eAUTable<eTable<ProgramMapSection> > m_PMT;
 	eAUTable<eTable<ProgramAssociationSection> > m_PAT;
-
-	ePtr<eConnection> m_SDTConn;
-	ePtr<iDVBSectionReader> m_SDTReader;
-	void readSDTdata(const __u8 *data, int len);
+	eAUTable<eTable<ApplicationInformationSection> > m_AIT;
+	eAUTable<eTable<OCSection> > m_OC;
 
 	eUsePtr<iDVBChannel> m_channel;
 	eUsePtr<iDVBPVRChannel> m_pvr_channel;
 	ePtr<eDVBResourceManager> m_resourceManager;
-	ePtr<iDVBDemux> m_demux;
-	bool m_first_tune;
+	ePtr<iDVBDemux> m_demux, m_pvr_demux_tmp;
 
 	void channelStateChanged(iDVBChannel *);
 	ePtr<eConnection> m_channelStateChanged_connection;
@@ -111,13 +89,21 @@ class eDVBServicePMTHandler: public sigc::trackable
 	ePtr<eConnection> m_channelEvent_connection;
 	void SDTScanEvent(int);
 	ePtr<eConnection> m_scan_event_connection;
-	void channelSourceEvent(int, std::string);
-	ePtr<eConnection> m_channelSourceEvent_connection;
+
+	void registerCAService();
 
 	void PMTready(int error);
 	void PATready(int error);
-	
+	void AITready(int error);
+	void OCready(int error);
+
 	int m_pmt_pid;
+	int m_dsmcc_pid;
+	int m_ait_pid;
+	HbbTVApplicationInfoList m_HbbTVApplications;
+	std::string m_HBBTVUrl;
+	std::string m_ApplicationName;
+	unsigned char m_AITData[4096];
 
 	int m_use_decode_demux;
 	uint8_t m_decode_demux_num;
@@ -132,8 +118,6 @@ private:
 	eDVBServicePMTHandler();
 public:
 #endif
-	static void setDDPSupport(bool b) { ddp_support = b; }
-	static void setTrueHDSupport(bool b) { truehd_support = b; }
 
 	enum
 	{
@@ -143,115 +127,73 @@ public:
 		eventNoPATEntry,   // no pat entry for the corresponding SID could be found
 		eventNoPMT,        // no pmt could be received (timeout)
 		eventNewProgramInfo, // we just received a PMT
-		eventUpdateDecoder, // is sent directly after tune start to speedup picture view...
 		eventTuned,        // a channel was sucessfully (re-)tuned in, you may start additional filters now
-		eventNewSDT,
 
 		eventPreStart,     // before start filepush thread
 		eventSOF,          // seek pre start
 		eventEOF,          // a file playback did end
-		
+
 		eventMisconfiguration, // a channel was not found in any list, or no frontend was found which could provide this channel
+
+		eventHBBTVInfo, /* HBBTV information was detected in the AIT */
+
+		eventStopped,
 	};
 #ifndef SWIG
 	sigc::signal1<void,int> serviceEvent;
-	sigc::signal2<void,int,std::string> sourceEvent;
-
-	struct videoStream
-	{
-		int pid;
-		int component_tag;
-		enum { vtMPEG2, vtMPEG4_H264, vtMPEG1, vtMPEG4_Part2, vtVC1, vtVC1_SM, vtH265 };
-		int type;
-	};
-	
-	struct audioStream
-	{
-		int pid,
-		    rdsPid; // hack for some radio services which transmit radiotext on different pid (i.e. harmony fm, HIT RADIO FFH, ...)
-		int type; // mpeg2, ac3, dts, ... as defined in iAudioType_ENUMS
-		int component_tag;
-		std::string language_code; /* iso-639, if available. */
-	};
-
-	struct subtitleStream
-	{
-		int pid;
-		int subtitling_type;  	/*  see ETSI EN 300 468 table 26 component_type
-									when stream_content is 0x03
-									0x10..0x13, 0x20..0x23 is used for dvb subtitles
-									0x01 is used for teletext subtitles */
-		union
-		{
-			int composition_page_id;  // used for dvb subtitles
-			int teletext_page_number;  // used for teletext subtitles
-		};
-		union
-		{
-			int ancillary_page_id;  // used for dvb subtitles
-			int teletext_magazine_number;  // used for teletext subtitles
-		};
-		std::string language_code;
-		bool operator<(const subtitleStream &s) const
-		{
-			if (pid != s.pid)
-				return pid < s.pid;
-			if (teletext_page_number != s.teletext_page_number)
-				return teletext_page_number < s.teletext_page_number;
-			return teletext_magazine_number < s.teletext_magazine_number;
-		}
-	};
-
-	struct aitStream
-	{
-		int pid;
-		std::vector<std::pair<int, int> > type_version_pairs;
-	};
-
-	struct program
-	{
-		struct capid_pair
-		{
-			uint16_t caid;
-			int capid;
-			bool operator< (const struct capid_pair &t) const { return t.caid < caid; }
-		};
-		std::vector<videoStream> videoStreams;
-		std::vector<audioStream> audioStreams;
-		int savedAudioStream;
-		std::vector<subtitleStream> subtitleStreams;
-		std::vector<aitStream> aitStreams;
-		std::list<capid_pair> caids;
-		int pcrPid;
-		int pmtPid;
-		int textPid;
-		bool isCrypted() { return !caids.empty(); }
-		std::map<std::string, boost::any> createDataMap(bool includeAitEit=true);
-	};
 
 	int getProgramInfo(program &program);
 	int getDataDemux(ePtr<iDVBDemux> &demux);
 	int getDecodeDemux(ePtr<iDVBDemux> &demux);
-	boost::any getCaIds(bool pair=false); // caid / ecmpid pair
-	
+	void getAITApplications(std::map<int, std::string> &aitlist);
+	void getCaIds(std::vector<int> &caids, std::vector<int> &ecmpids, std::vector<std::string> &ecmdatabytes);
+	PyObject *getHbbTVApplications();
+
 	int getPVRChannel(ePtr<iDVBPVRChannel> &pvr_channel);
 	int getServiceReference(eServiceReferenceDVB &service) { service = m_reference; return 0; }
 	int getService(ePtr<eDVBService> &service) { service = m_service; return 0; }
 	int getPMT(ePtr<eTable<ProgramMapSection> > &ptr) { return m_PMT.getCurrent(ptr); }
 	int getChannel(eUsePtr<iDVBChannel> &channel);
+	int getDemuxID() const { return m_decode_demux_num; }
 	void resetCachedProgram() { m_have_cached_program = false; }
 	void sendEventNoPatEntry();
+	void getHBBTVUrl(std::string &ret) const { ret = m_HBBTVUrl; }
+
+	enum serviceType
+	{
+		livetv = 0,
+		recording = 1,
+		scrambled_recording = 2,
+		playback = 3,
+		timeshift_recording = 4,
+		scrambled_timeshift_recording = 5,
+		timeshift_playback = 6,
+		streamserver = 7,
+		scrambled_streamserver = 8,
+		streamclient = 9,
+		offline = 10
+	};
 
 	/* deprecated interface */
-	int tune(eServiceReferenceDVB &ref, int use_decode_demux, eCueSheet *sg=0, bool simulate=false, eDVBService *service = 0);
+	int tune(eServiceReferenceDVB &ref, int use_decode_demux, eCueSheet *sg=0, bool simulate=false, eDVBService *service = 0, serviceType type = livetv, bool descramble = true);
 
 	/* new interface */
-	int tuneExt(eServiceReferenceDVB &ref, int use_decode_demux, sigc::slot1<ePtr<iTsSource>, const eServiceReferenceDVB&> createTsSource, const char *streaminfo_file, eCueSheet *sg=0, bool simulate=false, eDVBService *service = 0);
+	int tuneExt(eServiceReferenceDVB &ref, ePtr<iTsSource> &, const char *streaminfo_file, eCueSheet *sg=0, bool simulate=false, eDVBService *service = 0, serviceType type = livetv, bool descramble = true);
 
 	void free();
 private:
 	bool m_have_cached_program;
 	program m_cached_program;
+	serviceType m_service_type;
+
+	struct aitInfo
+	{
+		int id;
+		std::string url;
+		std::string name;
+	};
+	std::vector<struct aitInfo> m_aitInfoList;
+	int compareAudioSubtitleCode(const std::string &subtitleTrack, const std::string &audioTrack);
 #endif
 };
 
