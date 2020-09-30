e02fcf1c0a470cd86ff7b08fdd95ce470ca81636
thestormbreaker <33662974+thestormbreaker@users.noreply.github.com>
2020-09-30 08:26:00 +0200

    Update idemux.h

--- lib/dvb/idemux.h
+++ lib/dvb/idemux.h
@@ -8,7 +8,6 @@ class iDVBSectionReader: public iObject
 public:
 	virtual RESULT setBufferSize(int size)=0;
 	virtual RESULT start(const eDVBSectionFilterMask &mask)=0;
-	virtual RESULT startWithExternalBufferFunc(const eDVBSectionFilterMask &mask, const Slot0<__u8*> &buffer_func)=0;
 	virtual RESULT stop()=0;
 	virtual RESULT connectRead(const sigc::slot1<void,const uint8_t*> &read, ePtr<eConnection> &conn)=0;
 	virtual ~iDVBSectionReader() { };
